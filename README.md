b16da912756399d9ed098149b83ba76a940462ac
Renaud Martinet <karouf@gmail.com>
2014-02-06 15:34:46 +0100

    Generated new cookbook

---
+++ README.md
@@ -0,0 +1,13 @@
+# apps cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<karouf@gmail.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'apps'
+maintainer       'YOUR_NAME'
+maintainer_email 'karouf@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures apps'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
